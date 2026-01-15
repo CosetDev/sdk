015b55fb11aebc5201f1ae98c3c7a4973740071d
BeycanDeveloper <halilbeycan0@gmail.com>
2026-01-15 22:58:48 +0800

    added esbuild for cjs and esm

---
+++ esbuild.ts
@@ -0,0 +1,53 @@
+import glob from "fast-glob";
+import { execSync } from "child_process";
+import { build, BuildOptions } from "esbuild";
+import esbuildPluginTsc from "esbuild-plugin-tsc";
+
+async function generateTypes() {
+    try {
+        execSync("tsc --p tsconfig.build.json", { stdio: "inherit" });
+    } catch (error) {
+        console.error("Error generating type declarations:", error);
+        process.exit(1);
+    }
+}
+
+generateTypes();
+(async () => {
+    const files = await glob(["src/**/*.ts"], { ignore: ["**/*.d.ts"] });
+
+    const entryPoints = files.map(file => ({
+        in: file,
+        out: file.replace(/^src\//, "").replace(/\.ts$/, ""),
+    }));
+
+    const buildOptions: BuildOptions = {
+        bundle: false,
+        minify: true,
+        sourcemap: true,
+        logLevel: "error",
+        globalName: "CosetSDK",
+        entryPoints,
+        plugins: [
+            esbuildPluginTsc({
+                force: true,
+            }),
+        ],
+    };
+
+    await build(
+        Object.assign(buildOptions, {
+            format: "esm",
+            platform: "browser",
+            outdir: "dist/esm",
+        }) as BuildOptions
+    ).catch(() => process.exit(1));
+
+    await build(
+        Object.assign(buildOptions, {
+            format: "cjs",
+            platform: "node",
+            outdir: "dist/cjs",
+        }) as BuildOptions
+    ).catch(() => process.exit(1));
+})();

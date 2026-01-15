bc42f9b3c01f4dd136774134bc081973a7082cb2
BeycanDeveloper <halilbeycan0@gmail.com>
2026-01-15 22:17:44 +0800

    version up

--- test/index.test.ts
+++ test/index.test.ts
@@ -4,7 +4,7 @@ import { Coset, Networks, PaymentToken } from "../src/coset";
 
 dotenv.config({ path: ".env.test" });
 
-const TEST_ORACLE = "0xe8F71fE8bf0B11beF1C4179202c5c7c06E2eEcF6";
+const TEST_ORACLE = "0xfdF1DF1F5208982a47ce579cB5a7a4DA3126DEE1";
 const USER_PRIVATE_KEY = process.env.USER_PRIVATE_KEY as `0x${string}`;
 
 describe("Coset SDK", () => {
